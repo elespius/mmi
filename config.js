7ee2bd55738c4f7babf04a9bec0e9ce2b1d8d213
elespius <144125119+elespius@users.noreply.github.com>
2024-04-25 08:00:07 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,21 +1,9 @@
-// Add your database configuration here
-// Example configuration for MySQL using Node.js and mysql package:
+// Database configuration
+const config = {
+    host: 'localhost',
+    user: 'username',
+    password: 'password',
+    database: 'dbname'
+};
 
-const mysql = require('mysql');
-
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'username',
-  password: 'password',
-  database: 'database_name'
-});
-
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to database: ' + err.stack);
-    return;
-  }
-  console.log('Connected to database as id ' + connection.threadId);
-});
-
-module.exports = connection;
+module.exports = config;
